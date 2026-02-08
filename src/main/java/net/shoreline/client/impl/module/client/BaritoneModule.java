e0ac1f99bdc75c414405aa5b8428c773c2f5700c
3 <168254582+220814@users.noreply.github.com>
2026-02-08 07:15:42 +0700

    Update BaritoneModule.java

--- src/main/java/net/shoreline/client/impl/module/client/BaritoneModule.java
+++ src/main/java/net/shoreline/client/impl/module/client/BaritoneModule.java
@@ -1,6 +1,5 @@
 package net.shoreline.client.impl.module.client;
 
-import baritone.api.BaritoneAPI;
 import net.minecraft.text.Text;
 import net.shoreline.client.api.config.Config;
 import net.shoreline.client.api.config.setting.BooleanConfig;
@@ -14,7 +13,6 @@ import net.shoreline.client.impl.event.world.LoadWorldEvent;
 import net.shoreline.client.util.FormattingUtil;
 import net.shoreline.client.util.chat.ChatUtil;
 import net.shoreline.eventbus.annotation.EventListener;
-import net.shoreline.eventbus.event.StageEvent;
 
 import java.awt.*;
 
@@ -24,7 +22,7 @@ import java.awt.*;
  */
 public class BaritoneModule extends ConcurrentModule
 {
-
+    // useless
     Config<Float> rangeConfig = register(new NumberConfig<>("Range", "Baritone block reach distance", 0.0f, 4.0f, 5.0f));
     Config<Boolean> placeConfig = register(new BooleanConfig("Place", "Allow baritone to place blocks", true));
     Config<Boolean> breakConfig = register(new BooleanConfig("Break", "Allow baritone to break blocks", true));
@@ -52,91 +50,33 @@ public class BaritoneModule extends ConcurrentModule
     Config<Color> pathColor = register(new ColorConfig("CurrentPathColor", "The color of the path", Color.RED, false, false));
     Config<Color> nextPathColor = register(new ColorConfig("NextPathColor", "The color of the path", Color.MAGENTA, false, false));
 
-    /**
-     *
-     */
     public BaritoneModule()
     {
         super("Baritone", "Configure baritone", ModuleCategory.CLIENT);
     }
 
+    @Override
+    public void onEnable()
+    {
+        System.out.println("Baritone has been removed");
+    }
+
     @EventListener
     public void onConfigUpdate(ConfigUpdateEvent event)
     {
-        if (event.getStage() != StageEvent.EventStage.POST || event.getConfig().getContainer() == this)
-        {
-            return;
-        }
-        BaritoneAPI.getSettings().blockReachDistance.value = rangeConfig.getValue();
-        BaritoneAPI.getSettings().allowPlace.value = placeConfig.getValue();
-        BaritoneAPI.getSettings().allowBreak.value = breakConfig.getValue();
-        BaritoneAPI.getSettings().allowSprint.value = sprintConfig.getValue();
-        BaritoneAPI.getSettings().allowInventory.value = inventoryConfig.getValue();
-        BaritoneAPI.getSettings().allowVines.value = vinesConfig.getValue();
-        BaritoneAPI.getSettings().allowJumpAt256.value = jump256Config.getValue();
-        BaritoneAPI.getSettings().allowWaterBucketFall.value = waterBucketFallConfig.getValue();
-        BaritoneAPI.getSettings().allowParkour.value = parkourConfig.getValue();
-        BaritoneAPI.getSettings().allowParkourAscend.value = parkourAscendConfig.getValue();
-        BaritoneAPI.getSettings().allowParkourPlace.value = parkourPlaceConfig.getValue();
-        BaritoneAPI.getSettings().allowDiagonalAscend.value = diagonalAscendConfig.getValue();
-        BaritoneAPI.getSettings().allowDiagonalDescend.value = diagonalDescendConfig.getValue();
-        BaritoneAPI.getSettings().allowDownward.value = mineDownConfig.getValue();
-        BaritoneAPI.getSettings().legitMine.value = legitMineConfig.getValue();
-        BaritoneAPI.getSettings().disconnectOnArrival.value = logOnArrivalConfig.getValue();
-        BaritoneAPI.getSettings().freeLook.value = freeLookConfig.getValue();
-        BaritoneAPI.getSettings().antiCheatCompatibility.value = antiCheatConfig.getValue();
-        BaritoneAPI.getSettings().strictLiquidCheck.value = strictLiquidConfig.getValue();
-        BaritoneAPI.getSettings().censorCoordinates.value = censorCoordsConfig.getValue();
-        BaritoneAPI.getSettings().censorRanCommands.value = censorCommandsConfig.getValue();
-        BaritoneAPI.getSettings().chatControl.value = chatControlConfig.getValue();
-        BaritoneAPI.getSettings().chatDebug.value = debugConfig.getValue();
-        BaritoneAPI.getSettings().colorGoalBox.value = goalColor.getValue();
-        BaritoneAPI.getSettings().colorCurrentPath.value = pathColor.getValue();
-        BaritoneAPI.getSettings().colorMostRecentConsidered.value = pathColor.getValue();
-        BaritoneAPI.getSettings().colorNextPath.value = nextPathColor.getValue();
+        // Baritone logic removed 
     }
     
     @EventListener
     public void onChat(ChatMessageEvent event)
     {
-        syncBaritoneSettings();
+        // syncBaritoneSettings removed
     }
 
     @EventListener
     public void onLoadWorld(LoadWorldEvent event)
     {
-        syncBaritoneSettings();
-    }
-
-    private void syncBaritoneSettings()
-    {
-        rangeConfig.setValue(BaritoneAPI.getSettings().blockReachDistance.value);
-        placeConfig.setValue(BaritoneAPI.getSettings().allowPlace.value);
-        breakConfig.setValue(BaritoneAPI.getSettings().allowBreak.value);
-        sprintConfig.setValue(BaritoneAPI.getSettings().allowSprint.value);
-        inventoryConfig.setValue(BaritoneAPI.getSettings().allowInventory.value);
-        vinesConfig.setValue(BaritoneAPI.getSettings().allowVines.value);
-        jump256Config.setValue(BaritoneAPI.getSettings().allowJumpAt256.value);
-        waterBucketFallConfig.setValue(BaritoneAPI.getSettings().allowWaterBucketFall.value);
-        parkourConfig.setValue(BaritoneAPI.getSettings().allowParkour.value);
-        parkourAscendConfig.setValue(BaritoneAPI.getSettings().allowParkourAscend.value);
-        parkourPlaceConfig.setValue(BaritoneAPI.getSettings().allowParkourPlace.value);
-        diagonalAscendConfig.setValue(BaritoneAPI.getSettings().allowDiagonalAscend.value);
-        diagonalDescendConfig.setValue(BaritoneAPI.getSettings().allowDiagonalDescend.value);
-        mineDownConfig.setValue(BaritoneAPI.getSettings().allowDownward.value);
-        legitMineConfig.setValue(BaritoneAPI.getSettings().legitMine.value);
-        logOnArrivalConfig.setValue(BaritoneAPI.getSettings().disconnectOnArrival.value);
-        freeLookConfig.setValue(BaritoneAPI.getSettings().freeLook.value);
-        antiCheatConfig.setValue(BaritoneAPI.getSettings().antiCheatCompatibility.value);
-        strictLiquidConfig.setValue(BaritoneAPI.getSettings().strictLiquidCheck.value);
-        censorCoordsConfig.setValue(BaritoneAPI.getSettings().censorCoordinates.value);
-        censorCommandsConfig.setValue(BaritoneAPI.getSettings().censorRanCommands.value);
-        chatControlConfig.setValue(BaritoneAPI.getSettings().chatControl.value);
-        debugConfig.setValue(BaritoneAPI.getSettings().chatDebug.value);
-        goalColor.setValue(BaritoneAPI.getSettings().colorGoalBox.value);
-        pathColor.setValue(BaritoneAPI.getSettings().colorCurrentPath.value);
-        pathColor.setValue(BaritoneAPI.getSettings().colorMostRecentConsidered.value);
-        nextPathColor.setValue(BaritoneAPI.getSettings().colorNextPath.value);
+        // syncBaritoneSettings removed
     }
 
     @EventListener
@@ -150,3 +90,4 @@ public class BaritoneModule extends ConcurrentModule
         }
     }
 }
+                                                                 
